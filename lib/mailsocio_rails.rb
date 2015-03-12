f9594de7d90ada4d8addcabc9f2c0441f6b510b6
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2015-03-12 16:38:31 +0300

    Make a gem.

--- lib/mailsocio_rails.rb
+++ lib/mailsocio_rails.rb
@@ -1,4 +1,8 @@
-require "mailsocio/action/mailer/version"
+require 'actionmailer'
+require 'mailsocio_rails/delivery'
+require 'mailsocio_rails/version'
+
+ActionMailer::Base.add_delivery_method :mailsocio, MailsocioRails::Delivery
 
 module MailsocioRails
 end
---
+++ lib/mailsocio_rails/delivery.rb
@@ -0,0 +1,38 @@
+module MailsocioRails
+  class Delivery < ::Mail::SMTP
+    attr_accessor :settings
+    def initialize(settings)
+      self.settings = default_settings.merge settings
+    end
+
+    def deliver!(mail)
+      addrs = addresses mail
+      mail.to = settings[:mailsocio_recipient]
+
+      mail['X-To'] = Hash[addrs.map {|(addr, name)| [addr, {'name' => name}]}].to_json
+      mail['X-Account-Id'] = settings[:account_id]
+      mail['X-Api-Key'] = settings[:api_key]
+
+      super(mail)
+    end
+
+    private
+
+    def default_settings
+      {
+        address:             'app.sendbrain.com',
+        port:                25,
+        mailsocio_recipient: 'submit@app.sendbrain.com',
+        domain:              'localhost.localdomain'
+      }
+    end
+
+    def addresses(mail)
+      addresses_from(mail, :to) + addresses_from(mail, :cc)
+    end
+
+    def addresses_from(mail, method)
+      (mail.send(method) || []).zip(mail[method].try(:display_names) || [])
+    end
+  end
+end
